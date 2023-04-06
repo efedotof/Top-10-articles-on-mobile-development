6e636c64b397070ee47a38fa527dbe5e0bc515c0
Egor <80205178+efedotof@users.noreply.github.com>
2023-04-06 10:02:45 +0300

    add new design card

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:web_scraper/web_scraper.dart';
+import 'package:top10stat/screensplash.dart';
+import 'package:top10stat/array_list.dart';
 
 class Home_page extends StatefulWidget {
   const Home_page({super.key});
@@ -22,37 +25,57 @@ class _Home_pageState extends State<Home_page> {
     return ListView(
       padding: const EdgeInsets.all(3.14),
       children: [
-        for (int i = 0; i < 30; i++)
+        for (int i = 0; i < titleList.length; i++)
           Padding(
             padding: const EdgeInsets.only(top: 10),
-            child: card_space(context),
+            child: card_space(context, titleList[i]),
           ),
       ],
     );
   }
 
-  Widget card_space(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width - 10,
-      height: 150,
-      child: Card(
-        elevation: 3,
-        color: Colors.orange,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Название карточки",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: out_button_in_card(context),
-            ),
-          ],
+  Widget card_space(BuildContext context, text) {
+    return Padding(
+      padding: EdgeInsets.only(top: 10),
+      child: Container(
+        height: 300,
+        child: Card(
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 169, 184, 207),
+              Color.fromARGB(255, 197, 214, 240),
+            ])),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 40, left: 10),
+                      child: Text(
+                        "$text",
+                        style: TextStyle(color: Colors.white, fontSize: 19),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Читать...',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(
+                          color: Colors.transparent,
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
+          ),
         ),
       ),
     );
@@ -68,6 +91,16 @@ class _Home_pageState extends State<Home_page> {
             style: TextStyle(color: Colors.black12),
           )),
     );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (titleList.length == 0) {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (BuildContext context) => splashScreens()));
+    }
   }
 
   @override
